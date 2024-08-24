ee2103c18b1f9387e309d5dad91d742060e70edd
catchnaren <narenckumar@gmail.com>
2024-08-25 00:44:11 +0530

    Implement custom retriever with redundant vector filtering

--- prompt.py
+++ prompt.py
@@ -2,7 +2,11 @@ from langchain.vectorstores.chroma import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
+from redundant_filter_retriever import RedundantFilterRetriever
 from dotenv import load_dotenv
+import langchain
+
+langchain.debug = True
 
 load_dotenv()
 
@@ -15,7 +19,10 @@ db = Chroma(
   embedding_function = embeddings
 )
 
-retriever = db.as_retriever()
+retriever = RedundantFilterRetriever(
+  embeddings = embeddings,
+  chroma = db
+)
 
 chain = RetrievalQA.from_chain_type(
   llm = chat,
